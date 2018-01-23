850e6aad10adf57908f3ff7f79aee660645e6e7f
Kara Capozzi <karacapozzi@Karas-MacBook.local>
2018-01-23 14:50:56 -0500

    technical documentation assignment complete

---
+++ technical_documentation.rb
@@ -0,0 +1,28 @@
+# String
+
+length: finds the length of a string
+
+strip: removes whitespace before a string or after the string
+
+split(arg): finds the argument, splits the string or array and creates new versions of the values 'in between' the arg.
+
+start_with?(arg): returns a boolean of whether the string starts with the argument string
+
+# Array
+first(n): returns first n elements of an array.
+pop(n): removes last n from an array
+
+
+
+# Hash
+to_a: converts the hash to an array of [key, value] arrays
+has_key?: true if given key is in the hash
+
+
+# Time
+now: creates a time object for the current time
+
+
+# File
+exist?(arg): true if named file exists.
+extname: returns file extension.
